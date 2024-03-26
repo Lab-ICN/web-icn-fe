851177f6a5813fc7a1307185d5d9751787b2272e
Daffadon <daffaputranarendra9@gmail.com>
2024-03-27 03:14:16 +0700

    UI: Add Profile Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import MainLayout from "@/layout/MainLayout";
 import jumbotronBg from "../assets/image/jumbotron-bg.png"
+import profilBg from "../assets/image/profil-bg.png"
 import { Button } from "@/components/button/Button";
 export default function Home() {
   return (
@@ -19,10 +20,18 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div>
-
-        </div>
       </section >
+      <section id="profil" className="flex justify-center mt-16">
+        <div className="flex gap-20 justify-center items-center w-10/12">
+          <Image src={profilBg} alt="Profil" className="w-6/12" />
+          <div>
+            <h1 className="text-primary-blue font-semibold mb-2">Profil Laboratorium</h1>
+            <p className="font-light w-10/12">Laboratorium Jaringan Berbasis Informasi pada saat ini berfokus pada 4 topik penelitian, yaitu Jaringan Sensor dan Nirkabel, Sistem Terdistribusi dan Komputasi Awan, Kriptografi dan Keamanan Informasi, serta Teknologi Infrastruktur.</p>
+            <Button variant="blue" text="Selengkapnya" classname="px-10 py-3 mt-10 rounded-lg"></Button>
+          </div>
+        </div>
+
+      </section>
     </MainLayout >
   );
 }
